c10d5398489ef231ffec654230f91bbc5b148998
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-12-29 10:11:01 +0545

    feat: use folium for displaying map

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+import folium
 # Create your views here.
 class IndexView(View):
     template_name = "index.html"
     def get(self, request, *args, **kwargs):
-        return render(request,self.template_name)
+        # create map object
+        m = folium.Map()
+        # get html representation of map
+        m = m._repr_html_()
+        context = {
+            "m":m
+        }
+        return render(request,self.template_name, context)
