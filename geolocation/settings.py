2867e90c22cee4e2f44db74266f669505519f92e
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-12-29 07:41:17 +0545

    feat: setup app and basic templates

--- geolocation/settings.py
+++ geolocation/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # local apps
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'geolocation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
